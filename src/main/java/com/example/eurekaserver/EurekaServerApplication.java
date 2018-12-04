0235fa378a4fb8ef10b083ec147506e04f5a87de
shennychacko <shennychacko80@gmail.com>
2018-12-04 16:14:14 -0700

    files changed

--- src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//Testing
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
